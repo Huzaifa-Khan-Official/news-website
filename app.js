0f9233c7da4776acefb88a30c2e195d27546d0e9
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2023-10-16 17:43:01 +0500

    started

---
+++ app.js
@@ -0,0 +1,32 @@
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const cards = document.querySelector(".cards");
+
+btn.addEventListener("click", () => {
+    cards.innerHTML = "";
+    const apiKey = "8c8d1dfc665a40c5affe11ea36632f31";
+    const apiUrl = "https://newsapi.org/v2/everything?from=2023-10-10&sortBy=publishedAt&q=";
+
+    fetch(apiUrl + input.value + `&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            const articles = data.articles;
+
+
+            articles.forEach(curNews => {
+                const newsImageUrl = curNews.urlToImage;
+                const newsTitle = curNews.title;
+                const newsDes = curNews.description;
+                cards.innerHTML += `
+            <div class="card">
+            <img src="${newsImageUrl}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${newsTitle}</h5>
+            <p class="card-text">${newsDes}</p>
+            </div>
+            </div>
+        `
+            });
+        })
+        .catch(err => console.log("araha hai"));
+})
